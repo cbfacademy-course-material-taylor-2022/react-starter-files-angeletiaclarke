a3251f04cd4fe643be0c485beb227098c4c6666d
Angeletia Padmore <113722579+angeletiaclarke@users.noreply.github.com>
2023-04-25 20:26:05 +0100

    react practice

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const name = '[YOUR NAME GOES HERE]';
+const name = 'Angeletia';
 
 function Profile(name){
     return <main id="content" role="main" className="base">
             <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
+              <p>Here is my bio, i love to cook, camp on a hilltop in Dorset and create things</p>
             </main>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
+root.render(Profile(name));
 
